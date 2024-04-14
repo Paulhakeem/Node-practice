c4f5c57409f53f7967e769725879a052fa277e9e
Paulhakeem <paulnyamawi18@gmail.com>
2024-04-14 20:53:39 +0300

    post request

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
-const express = require('express')
-const fs = require("fs")
-let app = express()
+const express = require("express");
+const fs = require("fs");
+let app = express();
 
-let movies = JSON.parse(fs.readFileSync('./express/movies.json'))
+let movies = JSON.parse(fs.readFileSync("./express/movies.json"));
 
+// Get request
+app.get("/api/v1/movies", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      movies: movies,
+    },
+  });
+});
 
-app.get('/api/v1/movies', (req, res) => {
+// POST request
+app.use(express.json());
+app.post("/api/v1/movies", (req, res) => {
+  // creating an ID for a new object
+  const newID = movies[movies.length - 1].id + 1;
+  // creating a new object
+  const newMovie = Object.assign({ id: newID }, req.body);
+  // push newmovie to movies
+  movies.push(newMovie);
+  // writing a new file
+  fs.writeFile("./express/movies.json", JSON.stringify(movies), (err) => {
     res.status(200).json({
-        "status": "success",
-        "data": {
-            movies: movies
-        }
-    })
-
-})
+      status: "sucess",
+      data: {
+        movies: newMovie,
+      },
+    });
+  });
+});
 
 app.listen(8080, () => {
-    console.log("Server in running");
-})
+  console.log("Server in running");
+});
