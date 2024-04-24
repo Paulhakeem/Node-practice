dd02a7bae3c2f3c0d531644bbecd028652a6c14e
Paulhakeem <paulnyamawi18@gmail.com>
2024-04-24 23:02:40 +0300

    creating a configartion file and read the evroment variable

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require("express"); // modules
+// modules
+const env = require('dotenv')
 const movieRouter = require('./express/movieRoute/route') // modules
 const morgan = require("morgan"); // modules
+const express = require("express"); 
 let app = express();
+
+env.config({path: "./config.env"})
+console.log(process.env);
 app.use(express.json()); // middleware
+app.use(express.static("./public"))
 app.use(morgan("dev"));// middleware
 
 app.use("/api/v1/movies",movieRouter) // CREATING route
