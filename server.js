cfaf419ab933363cfbddd6645dfd149db09057ff
Paulhakeem <paulnyamawi18@gmail.com>
2024-04-28 22:31:46 +0300

    connect node application with mongoDB

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 // modules
-const env = require('dotenv')
-const movieRouter = require('./express/movieRoute/route') // modules
+const env = require("dotenv");
+const mongoose = require("mongoose");
+const movieRouter = require("./express/movieRoute/route"); // modules
 const morgan = require("morgan"); // modules
-const express = require("express"); 
+const express = require("express");
 let app = express();
 
-env.config({path: "./config.env"})
-console.log(process.env);
+env.config({ path: "./config.env" });
 app.use(express.json()); // middleware
-app.use(express.static("./public"))
-app.use(morgan("dev"));// middleware
+app.use(express.static("./public"));
+app.use(morgan("dev")); // middleware
 
-app.use("/api/v1/movies",movieRouter) // CREATING route
+// connecting node with mongodb database
+mongoose
+  .connect(process.env.CONNECTION_STR, {})
+  .then((conn) => {
+    console.log("connection successful!!");
+  })
+  .catch((err) => {
+    console.log("error");
+  });
+
+//   creating a schema
+const movieSchema = new mongoose.Schema({
+    name: {type: String, required: [true, "Name required"]},
+    descrption: String,
+    duration: {type: Number, required: [true, "Duration requred"]},
+    rating: {type: Number, default: 1.0}
+})
+// creating a model
+const movie = mongoose.model('movie', movieSchema)
+
+
+
+app.use("/api/v1/movies", movieRouter); // CREATING route
 //   server
 app.listen(8080, () => {
   console.log("Server in running");
