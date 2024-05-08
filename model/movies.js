6eaff078a52cd711b49b1ec1a3bb00754ac46f9e
Paulhakeem <paulnyamawi18@gmail.com>
2024-05-08 22:45:16 +0300

    usage of virtual method

--- model/movies.js
+++ model/movies.js
@@ -1,14 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 //   creating a schema
-const movieSchema = new mongoose.Schema({
+const movieSchema = new mongoose.Schema(
+  {
     name: { type: String, required: [true, "Name required"], unique: true },
     descrption: String,
     duration: { type: Number, required: [true, "Duration requred"] },
     rating: { type: Number, default: 1.0 },
-  });
-  // creating a model/document
-  const Movie = mongoose.model("Movie", movieSchema);
- 
-  module.exports=Movie
-  
+  },
+  {
+    toJSON: {virtuals:true},
+    toObject: {virtuals:true},
+  }
+);
+// vitual function/method get duration in hours is not stored in database
+movieSchema.virtual("durationInHours").get(function(){
+  return this.duration / 60;
+});
+// creating a model/document
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
