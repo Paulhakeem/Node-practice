24cf8b9142daa23adcdbc0d8169ae49cfcd0a51b
Paulhakeem <paulnyamawi18@gmail.com>
2024-05-10 23:34:06 +0300

    using some build-in validations in mongoDB

--- model/movies.js
+++ model/movies.js
@@ -4,15 +4,42 @@ const fs = require("fs");
 //   creating a schema
 const movieSchema = new mongoose.Schema(
   {
-    name: { type: String, required: [true, "Name required"], unique: true },
+    name: {
+      type: String,
+      required: [true, "Name required"],
+      unique: true,
+      maxlength: [10, "movie length must be 10 or less"],
+      minlength: [2, "movie length should be atleast 2 or more"],
+    },
     descrption: String,
     duration: { type: Number, required: [true, "Duration requred"] },
-    rating: { type: Number, default: 1.0 },
+    rating: {
+      type: Number,
+      default: 1.0,
+      min: [1, "Rating should be 1 or more"],
+      max: [10, "Rating maximum should be 10"]
+    },
     createdAt: {
       type: Date,
       default: Date.now(),
     },
-    createdBy: String,
+    createdBy: { type: String },
+    catergory: {
+      type: String,
+      required: [true, "type of a movie required ie Movie or Series"],
+      enum: {
+        values: ["Movie", "Series"],
+        message: "Not recognizable"
+      }
+    },
+    genre: {
+      type: String,
+      required: [true, "genre required"],
+      enum: {
+        values: ["Action", "sci-fi", "adverture", "comedy", "History", "thriller", "Horror"],
+        message: "Not recognizable"
+      },
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -37,15 +64,15 @@ movieSchema.pre("save", function (next) {
 //   next();
 // });
 // CRATING A MIDDLEWARE BEFORE FINDING A MOVIE
-movieSchema.pre(/^find/, function(next){
-  this.find({rating: {$gte: 5}})
-  next()
-})
+movieSchema.pre(/^find/, function (next) {
+  this.find({ rating: { $gte: 5 } });
+  next();
+});
 // Aggregation middleware
-movieSchema.pre('aggregate', function(next){
-  console.log(this.pipeline().unshift({$match: {rating: {$gte: 5}}}));
-  next()
-})
+movieSchema.pre("aggregate", function (next) {
+  console.log(this.pipeline().unshift({ $match: { rating: { $gte: 5 } } }));
+  next();
+});
 // creating a model/document
 const Movie = mongoose.model("Movie", movieSchema);
 
