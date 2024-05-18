90cfadb59097192b03fffada125c6864e188c7c6
Paulhakeem <paulnyamawi18@gmail.com>
2024-05-18 22:06:25 +0300

    validation error handling

--- express/error/errorController.js
+++ express/error/errorController.js
@@ -27,14 +27,21 @@ const prodError = (res, error) => {
 // handling production error
 const castErrorHandle = (err) => {
   const msg = `Invalid value ${err.value} for filed ${err.path}`;
-  return new ErrorHandling(msg, 404);
+  return new ErrorHandling(msg, 400);
 };
 // dublicated error handling
-const dublicatedError = (err)=> {
-  const name = err.keyValue.name
-  const message = `${name} is dublicated. Please use another name`
+const dublicatedError = (err) => {
+  const name = err.keyValue.name;
+  const message = `${name} is dublicated. Please use another name`;
   console.log(name);
-  return new ErrorHandling(message, 404)
+  return new ErrorHandling(message, 400);
+};
+// validation error
+const validationError = (err)=> {
+  const allErrors = Object.values(err.errors).map(val => val.message)
+  const errorMessage = allErrors.join(". ")
+  const message = `validation error: ${errorMessage}`
+  return new ErrorHandling(message, 400)
 }
 
 module.exports = (error, req, res, next) => {
@@ -45,9 +52,10 @@ module.exports = (error, req, res, next) => {
     devError(res, error);
   } else if (process.env.NODE_ENV === "production") {
     if (error.name === "CastError") {
-        error = castErrorHandle(error);
-        console.log('hello');
-    }else if(error.code === 11000) error = dublicatedError(error)
+      error = castErrorHandle(error);
+      console.log("hello");
+    } else if (error.code === 11000) error = dublicatedError(error);
+    if ((error.name = "ValidationError")) error = validationError(error);
     prodError(res, error);
   }
   next();
