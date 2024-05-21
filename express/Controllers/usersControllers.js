cc9bd935af8d48ffd4414c6d20e5e0866df9fe6a
Paulhakeem <paulnyamawi18@gmail.com>
2024-05-22 00:11:47 +0300

    signup user using JWT

--- express/Controllers/usersControllers.js
+++ express/Controllers/usersControllers.js
@@ -1,10 +1,15 @@
 const Users = require("./../../model/users")
 const asyncErrorHandling = require("./../error/asynError");
+const jwt = require('jsonwebtoken')
 //create a new user / post request
 exports.createUser = asyncErrorHandling(async(req,res,next)=> {
     const newUser = await Users.create(req.body)
+    const userCredentials = jwt.sign({name: newUser._id}, process.env.SECRET_STR, {
+        expiresIn: process.env.EXP_DATE
+    })
     res.status(201).json({
         status: "success",
+        userCredentials,
         data: {
             user: newUser
         }
