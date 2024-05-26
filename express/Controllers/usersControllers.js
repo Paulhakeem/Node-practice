98217c345cb306f83e544131fd8a49478e75cb38
Paulhakeem <paulnyamawi18@gmail.com>
2024-05-26 11:52:57 +0300

    protecing the route by checking the passwords

--- express/Controllers/usersControllers.js
+++ express/Controllers/usersControllers.js
@@ -1,74 +1,92 @@
-const Users = require("./../../model/users")
+const User = require("./../../model/users");
+const Users = require("./../../model/users");
 const asyncErrorHandling = require("./../error/asynError");
 const ErrorHandling = require("./../error/errorHanding");
-const errorHandling = require('./../error/errorHanding')
-const jwt = require('jsonwebtoken')
-const util = require("util")
+const errorHandling = require("./../error/errorHanding");
+const jwt = require("jsonwebtoken");
+const util = require("util");
 
 // function handling user
-const userTokens = (id)=>{
-return jwt.sign({id}, process.env.SECRET_STR, {
-    expiresIn: process.env.EXP_DATE
-})
-}
+const userTokens = (id) => {
+  return jwt.sign({ id }, process.env.SECRET_STR, {
+    expiresIn: process.env.EXP_DATE,
+  });
+};
 //create a new user / post request
-exports.createUser = asyncErrorHandling(async(req,res,next)=> {
-    const newUser = await Users.create(req.body)
-    const userCredentials = userTokens(newUser._id)
-    res.status(201).json({
-        status: "success",
-        userCredentials,
-        data: {
-            user: newUser
-        }
-    })
-    next()
-})
+exports.createUser = asyncErrorHandling(async (req, res, next) => {
+  const newUser = await Users.create(req.body);
+  const userCredentials = userTokens(newUser._id);
+  res.status(201).json({
+    status: "success",
+    userCredentials,
+    data: {
+      user: newUser,
+    },
+  });
+  next();
+});
 
 // login user
-exports.login = asyncErrorHandling(async(req, res, next)=> {
-    const {email, password} = req.body
-    // check if user provide the above info
-    if(!email || !password) {
-     const error = new errorHandling("Please provide your email and Password", 400)
-     return next(error)
-    }
+exports.login = asyncErrorHandling(async (req, res, next) => {
+  const { email, password } = req.body;
+  // check if user provide the above info
+  if (!email || !password) {
+    const error = new errorHandling(
+      "Please provide your email and Password",
+      400
+    );
+    return next(error);
+  }
 
-    // check the user info & filter
-    const loginUser = await Users.findOne({email}).select("+password")
-    const isMatch = await loginUser.comparePasswordInDB(password, loginUser.password)
-    // check user
-    if(!loginUser || !isMatch){
-        const userError = new ErrorHandling("Incorrect Email or Password", 400)
-        return next(userError)
-    }
-    const tokens = userTokens(loginUser._id)
+  // check the user info & filter
+  const loginUser = await Users.findOne({ email }).select("+password");
+  const isMatch = await loginUser.comparePasswordInDB(
+    password,
+    loginUser.password
+  );
+  // check user
+  if (!loginUser || !isMatch) {
+    const userError = new ErrorHandling("Incorrect Email or Password", 400);
+    return next(userError);
+  }
+  const tokens = userTokens(loginUser._id);
 
-    res.status(200).json({
-        status: "success",
-        tokens,
-        loginUser
-    })
-})
+  res.status(200).json({
+    status: "success",
+    tokens,
+    loginUser,
+  });
+});
 
 // protecting routes
-exports.protectRoutes = asyncErrorHandling(async(req,res,next)=> {
-    // Read user token
-  const userToken = req.headers.authorization
+exports.protectRoutes = asyncErrorHandling(async (req, res, next) => {
+  // Read user token
+  const userToken = req.headers.authorization;
   let token;
-  if(userToken && userToken.startsWith("paul")){
-    token = userToken.split(" ")[1]
+  if (userToken && userToken.startsWith("paul")) {
+    token = userToken.split(" ")[1];
   }
-  if(!token){
-    const error = errorHandling("Invalid token", 401)
+  if (!token) {
+    const error = errorHandling("Invalid token", 401);
+    next(error);
+  }
+  // validate tokens
+  const promiseToken = await util.promisify(jwt.verify)(
+    token,
+    process.env.SECRET_STR
+  );
+  // if user exist
+  const userExist = await User.findById(promiseToken.id);
+if(!userExist){
+    const error = new errorHandling("User does not exist!. Please check your email and password", 401)
+    next(error)
+}
+  // if user change the password
+  if(userExist.isPasswordChanged(promiseToken.iat)){
+    const error = new errorHandling("The password has been change!, Please login again", 401)
     next(error)
   }
-    // validate tokens
-   const promiseToken = await util.promisify(jwt.verify)(token, process.env.SECRET_STR)
-   console.log(promiseToken);
 
-    // if user change the password 
-
-    // allow user to access the route
-    next()
-})
+  // allow user to access the route
+  next();
+});
--- model/users.js
+++ model/users.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
 // user schem
 const userSchema = new mongoose.Schema({
@@ -15,40 +15,57 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please enter your email address."],
     validate: [validator.isEmail],
   },
-  password:{
+  password: {
     type: String,
     required: [true, "Please enter your password"],
     miniLength: [8, "Password shuld be of 8 characters"],
-    select: false
+    select: false,
   },
   confirmPassword: {
     type: String,
     require: [true, "Please confirm your password"],
     validate: {
-      validator: function(val){
-        return val == this.password
-       },
-       message: "Confirm password does not match!."
+      validator: function (val) {
+        return val == this.password;
+      },
+      message: "Confirm password does not match!.",
     },
-    select: false
-  }
-
+    select: false,
+  },
+  passwordChangeAt: Date,
 });
 
 // encrypt password
-userSchema.pre("save", async function(next){
-// encypt password before saving it
-    this.password = await bcrypt.hash(this.password, 12)
-    this.confirmPassword = undefined
-    next()
-})
+userSchema.pre("save", async function (next) {
+  // encypt password before saving it
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
+});
 
 // compare password wit db password
-userSchema.methods.comparePasswordInDB = async function(pswd, pswdDB){
-  return await bcrypt.compare(pswd, pswdDB)
-}
+userSchema.methods.comparePasswordInDB = async function (pswd, pswdDB) {
+  return await bcrypt.compare(pswd, pswdDB);
+};
+
+// if user change password
+userSchema.methods.isPasswordChanged = async function (JWTtimeStamp) {
+  if (this.passwordChangeAt) {
+    // changing passwordchangeAt to seconds
+    const pwdAtToTimestamp = parseInt(
+      this.passwordChangeAt.getTime() / 1000,
+      10
+    );
+
+    if (pwdAtToTimestamp) {
+      return JWTtimeStamp < pwdAtToTimestamp;
+    }
+    console.log(pwdAtToTimestamp, JWTtimeStamp);
+  }
+  return false;
+};
 
 // model
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
